ee46563e7c4344aa2b2ccd95090cc909ae342ae6
romger@yandex-team.ru <soft-marketlab@yandex-team.ru>
2021-05-27 15:32:33 +0300

    решил последнюю задачу

--- курс по алгоритмам/спринт 8 - алгоритмы на строках/финальные задачи/B. Шпаргалка/app.js
+++ курс по алгоритмам/спринт 8 - алгоритмы на строках/финальные задачи/B. Шпаргалка/app.js
@@ -1,10 +1,34 @@
+// ID - 51680123
+
+/**
+ * Принцип работы алогритма:
+ * 1. Собираем дерево слов из словаря, добавляем в него метод, который проверяет любое слово на наличие в словаре
+ * 2. С помощью динамического программирования проходим по всему тексту посимвольно
+ *      - Если от начала текста до проверяемого символа букв не больше, чем в самом длинном слове из словаря, то проверяем весь кусок [0, i] на наличие в словаре, если он там есть, то в dp[i] записываем true
+ *      - Дальше идём назад по массиву dp, пока не отойдём от проверяемого индекса на расстояние равное самому длинному слову из словаря
+ *          - на каждой итерации проверяем dp[checkI], если оно true, значит проверяем кусок [checkI + 1, i] на наличие в словаре, если он там есть, то в dp[i] записываем true
+ * 3. в dp[text.length - 1] будет лежать ответ
+ * 
+ * Временная сложность:
+ * 1. Построение дерева - O(L), где L - суммарная длина слов в словаре
+ * 2. Проверка каждого индекса в тексте: для каждого индекса мы проведём максимум 100 операций (наибольшая возможная длина слова в словаре, по условию)
+ *    В каждой из этих операций самая сложная часть - поиск части текста в словаре. Поиск слова в словаре равна O(MaxL) времени, где MaxL - максимальная длина слова в словаре
+ *    Получается проверка займёт O(N * MaxL * MaxL), так как по-условию длинна текста (N) не превосходит 10^5, а длина слова в словаре (MaxL) не превосходит 10^2, то MaxL*MaxL можно приравнять к N
+ * Получается временная сложность: O(L + N^2), где L - суммарная длина слов в словаре, а N - длинна текста
+ * 
+ * Пространственная сложность:
+ * 1. Весь текст O(N)
+ * 2. Все слова из словаря O(L)
+ * Получается пространственная сложность: O(L + N)
+ */
+
 const fs = require('fs');
 let fileContent = fs.readFileSync('input.txt', 'utf8');
  
 let text = '';
 let countWords = 0;
 let dex = 10;
- 
+let maxLengthWord = 0;
  
 class TrieItem {
     constructor(value, isTerm = false) {
@@ -18,10 +42,23 @@ class Trie {
     constructor() {
         this.root = new TrieItem('');
     }
+
+    hasWord(word) {
+        let currentNode = this.root;
+        for (let i = 0; i < word.length; i++) {
+            let symbol = word[i];
+            if (!currentNode.child.get(symbol)) {
+                return false;
+            }
+            currentNode = currentNode.child.get(symbol);
+        }
+        return currentNode.isTerm;
+    }
 }
  
 const trie = new Trie();
- 
+
+// добавить слово в дерево
 function addWordToTrie(word) {
     let currentNode = trie.root;
     for (let i = 0; i < word.length; i++) {
@@ -45,27 +82,46 @@ fileContent.split('\n').forEach((line, index) => {
     }
     if (index > 1 && index <= countWords + 1) {
         addWordToTrie(line);
+        // сохраняем максимальную длинну слова
+        if (line.length > maxLengthWord) {
+            maxLengthWord = line.length;
+        }
     }
 });
- 
-let stack = [{node: trie.root, index: 0, isRoot: true}];
- 
-let isValid = false;
- 
-while(stack.length && !isValid) {
-    const {node, index, isRoot} = stack.pop();
-    if (index >= text.length) {
-        isValid = node.isTerm || isRoot;
-        continue;
+
+// погнали проверять
+function check() {
+    // массив для хранения результатов ДП
+    let dp = new Array(text.length);
+    // идём по всем символам в тексте
+    for (let index = 0; index < dp.length; index++) {
+        // по-умалчанию считаем проверяемый отрезок не валидным
+        let valid = false;
+        // если до начала текста не дальше, чем саммое длинное слово, то проверим этот отрезок сразу
+        if (index + 1 <= maxLengthWord) {
+            valid = trie.hasWord(text.substring(0, index + 1));
+        }
+        // высчитываем насколько далеко имеет смысл заходить назад для проверки
+        let checkPrevIndex = index - 1;
+        let minCheckPrevIndex = index - maxLengthWord;
+        if (minCheckPrevIndex < 0) {
+            minCheckPrevIndex = 0;
+        }
+        // идём пошагово назад, пока не найдём валидную комбинацию или не дойдём до конца
+        while(!valid && checkPrevIndex >= minCheckPrevIndex) {
+            // если тут сохранена не валидная кобинация, то пропускаем этот индекс
+            if (!dp[checkPrevIndex]) {
+                checkPrevIndex--;
+                continue;
+            }
+            // иначе проверяем отрезок отсюда до нашего index
+            valid = trie.hasWord(text.substring(checkPrevIndex + 1, index + 1));
+            checkPrevIndex--;
+        }
+        dp[index] = valid;
     }
-    let nextNode = node.child.get(text[index]);
-    if (!nextNode) {
-        continue;
-    }
-    stack.push({node: nextNode, index: index + 1, isRoot: false});
-    if (nextNode.isTerm) {
-        stack.push({node: trie.root, index: index + 1, isRoot: true});
-    }
+    return dp[dp.length - 1];
 }
  
-fs.writeFileSync('output.txt', isValid ? 'YES' : 'NO');
+ 
+fs.writeFileSync('output.txt', check() ? 'YES' : 'NO');
